a5b181c0f1c8e1a81b5fdb5bb04b1b8170bf0f9f
Soham Basu <sohambasu@outlook.com>
2025-04-20 08:28:17 +0530

    linter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,57 @@
+plugins {
+  id("com.diffplug.spotless") version("6.25.0")
+}
+
 repositories {
   gradlePluginPortal()
 }
 
+spotless {
+  protobuf { buf() }
+
+  shell {
+    target("**/*.sh", "**/*.bash")
+    shfmt("3.10.0")
+  }
+
+  json {
+    target("**/*.json")
+    targetExclude("**/build/**")
+    endWithNewline()
+    gson().sortByKeys()
+  }
+
+  yaml {
+    target("**/*.yaml", "**/*.yml")
+    targetExclude("**/build/**")
+    endWithNewline()
+    trimTrailingWhitespace()
+    jackson().yamlFeature("LITERAL_BLOCK_STYLE", true)
+  }
+
+  java {
+    target("**/*.java", "**/*.java")
+    targetExclude("**/build/**")
+    googleJavaFormat()
+    formatAnnotations()
+    removeUnusedImports()
+  }
+
+  kotlin {
+    target("**/*.kt")
+    targetExclude("**/build/**")
+    ktfmt().googleStyle()
+  }
+
+  kotlinGradle {
+    target("**/*.gradle.kts")
+    targetExclude("**/build/**")
+    ktfmt().googleStyle()
+  }
+
+  sql {
+    target("**/*.sql")
+    targetExclude("**/build/**")
+    prettier()
+  }
+}
